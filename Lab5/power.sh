1ba0e478e06d0402a99cf3cb017f840ae7fb7047
hot blazer <49531779+hotblaze18@users.noreply.github.com>
2020-12-17 15:28:15 +0530

    Add files via upload

---
+++ Lab5/power.sh
@@ -0,0 +1,15 @@
+echo "POWER OF NUMBER \n"
+echo "Enter the number"
+read n
+echo "Enter the power"
+read p
+
+counter=0
+ans=1
+while [ $p -ne $counter ]
+do
+        ans=`expr $ans \* $n`
+        counter=`expr $counter + 1`
+done
+
+echo "$n power of $p is:" $ans
